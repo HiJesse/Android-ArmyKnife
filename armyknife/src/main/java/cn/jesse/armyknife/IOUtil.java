600144e83da20475ec6fa5b1d1e99e755ee2ca76
Jesse <lenjoyshow@gmail.com>
2017-07-16 16:13:59 +0800

    ftr(util): add io utils

---
+++ armyknife/src/main/java/cn/jesse/armyknife/IOUtil.java
@@ -0,0 +1,34 @@
+package cn.jesse.armyknife;
+
+import android.util.Log;
+
+import java.io.Closeable;
+
+/**
+ * IO相关工具
+ * Created by jesse on 16/07/2017.
+ */
+
+public class IOUtil {
+    private static final String TAG = IOUtil.class.getSimpleName();
+
+    private IOUtil() {
+        // unused
+    }
+
+    /**
+     * 正确关闭实现了Closeable的对象
+     * @param closeable 要关闭的对象
+     */
+    public static void close(Closeable closeable) {
+        if (closeable == null) {
+            return;
+        }
+
+        try {
+            closeable.close();
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+    }
+}
