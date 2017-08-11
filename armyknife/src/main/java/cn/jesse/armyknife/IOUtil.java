0a262f4f5d71db3e0bc52c1365d51f7f5d394212
Jesse <lenjoyshow@gmail.com>
2017-08-11 23:43:23 +0800

    ftr(file): add file util

--- armyknife/src/main/java/cn/jesse/armyknife/IOUtil.java
+++ armyknife/src/main/java/cn/jesse/armyknife/IOUtil.java
@@ -2,9 +2,18 @@ package cn.jesse.armyknife;
 
 import android.util.Log;
 
+import java.io.BufferedOutputStream;
 import java.io.Closeable;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import cn.jesse.armyknife.exception.UnsupportedOperationException;
+
+import static cn.jesse.armyknife.FileUtil.createOrExistsFile;
+import static cn.jesse.armyknife.FileUtil.getFileByPath;
 
 /**
  * IO相关工具
@@ -13,6 +22,7 @@ import cn.jesse.armyknife.exception.UnsupportedOperationException;
 
 public class IOUtil {
     private static final String TAG = IOUtil.class.getSimpleName();
+    private static int sBufferSize = 8192;
 
     private IOUtil() {
         throw new UnsupportedOperationException("cannot be instantiated");
@@ -21,17 +31,83 @@ public class IOUtil {
     /**
      * 正确关闭实现了Closeable的对象
      *
-     * @param closeable 要关闭的对象
+     * @param closeables 要关闭的对象
      */
-    public static void close(Closeable closeable) {
-        if (closeable == null) {
+    public static void close(final Closeable... closeables) {
+        if (closeables == null) {
             return;
         }
-
-        try {
-            closeable.close();
-        } catch (Exception e) {
-            Log.e(TAG, e.getMessage(), e);
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (Exception e) {
+                    Log.e(TAG, e.getMessage(), e);
+                }
+            }
         }
+    }
+
+    /**
+     * 将输入流写入文件
+     *
+     * @param filePath 路径
+     * @param is       输入流
+     * @return {@code true}: 写入成功<br>{@code false}: 写入失败
+     */
+    public static boolean writeFileFromIS(final String filePath, final InputStream is) {
+        return writeFileFromIS(getFileByPath(filePath), is, false);
+    }
+
+    /**
+     * 将输入流写入文件
+     *
+     * @param filePath 路径
+     * @param is       输入流
+     * @param append   是否追加在文件末
+     * @return {@code true}: 写入成功<br>{@code false}: 写入失败
+     */
+    public static boolean writeFileFromIS(final String filePath, final InputStream is, final boolean append) {
+        return writeFileFromIS(getFileByPath(filePath), is, append);
+    }
+
+    /**
+     * 将输入流写入文件
+     *
+     * @param file 文件
+     * @param is   输入流
+     * @return {@code true}: 写入成功<br>{@code false}: 写入失败
+     */
+    public static boolean writeFileFromIS(final File file, final InputStream is) {
+        return writeFileFromIS(file, is, false);
+    }
+
+    /**
+     * 将输入流写入文件
+     *
+     * @param file   文件
+     * @param is     输入流
+     * @param append 是否追加在文件末
+     * @return {@code true}: 写入成功<br>{@code false}: 写入失败
+     */
+    public static boolean writeFileFromIS(final File file, final InputStream is, final boolean append) {
+        if (!createOrExistsFile(file) || is == null) {
+            return false;
+        }
+        OutputStream os = null;
+        try {
+            os = new BufferedOutputStream(new FileOutputStream(file, append));
+            byte data[] = new byte[sBufferSize];
+            int len;
+            while ((len = is.read(data, 0, sBufferSize)) != -1) {
+                os.write(data, 0, len);
+            }
+            return true;
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage(), e);
+        } finally {
+            close(is, os);
+        }
+        return false;
     }
 }
